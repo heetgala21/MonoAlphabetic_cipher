418c98c6912279e5493ad5fbe1e526b010ecc22b
heetgala21 <43107650+heetgala21@users.noreply.github.com>
2020-08-09 20:52:59 +0530

    Update monoalpha.py

--- monoalpha.py
+++ monoalpha.py
@@ -1,22 +1,22 @@
 import random
 alpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-key = ''.join(random.sample(alpha,len(alpha)))
+key = ''.join(random.sample(alpha,len(alpha)))#generates a random shuffle of alphabets
 
 def encrypt(encrypt_msg):
         final_ans = ""
-        for char in encrypt_msg:
-                num = alpha.find(char)
-                final_ans = final_ans + key[num]
+        for char in encrypt_msg:#iterates over every character in text to be encrypted
+                num = alpha.find(char)#num = index of character
+                final_ans = final_ans + key[num]#append new char from random shuffle of alphabets
         print(final_ans)
 def decrypt(decrypt_msg):
         final_ans = ""
-        for char in decrypt_msg:
-                num = key.find(char)
-                final_ans = final_ans + alpha[num]
+        for char in decrypt_msg:#iterates over every character in text to be encrypted
+                num = key.find(char)#num = index of character
+                final_ans = final_ans + alpha[num]#append new char from alphabets
         print(final_ans)
 
 
-while True:
+while True:#Menu driven While loop; goes on untill EXIT option is not selected or program is not killed
         n = int(input("Enter value:: \n1) To Encrypt Text:: \n2) To Decrypt Text:: \n3) See Key:: \n4) To Exit \n"))
         if (n == 1):
              encrypt_msg = str(input("Enter Text:: "))
